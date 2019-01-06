aee1b3f14d418370b0a5f73255d4845812580ae1
mongkyo <dreamong91@gmail.com>
2019-01-06 23:24:47 +0900

    [Add] makemigration

--- app/blog/models.py
+++ app/blog/models.py
@@ -15,3 +15,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
