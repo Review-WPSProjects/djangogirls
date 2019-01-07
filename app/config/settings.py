072fc74d638175fc3a62b0031f0ec722f8666bb5
mongkyo <dreamong91@gmail.com>
2019-01-08 00:10:07 +0900

    [fix] models.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -31,11 +31,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Auth
+AUTH_USER_MODEL = 'members.User'
 
 # Application definition
 
 INSTALLED_APPS = [
     'blog',
+    'members',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -84,9 +87,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "djangogirls",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
